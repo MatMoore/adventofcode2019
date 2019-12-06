d1bc292650ad183b7bd5047863031f91d9651c95
Mat Moore <matmoore@users.noreply.github.com>
2019-12-06 11:53:30 +0000

    Add some tests
    To make sure I don't break code from previous days

--- tests/testthat/test-advent.R
+++ tests/testthat/test-advent.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("day 1 part 1 returns the correct answer", {
+  answer <- day_01_part1()
+  expect_equal(answer, 3456641)
+})
+
+test_that("day 1 part 2 returns the correct answer", {
+  answer <- day_01_part2()
+  expect_equal(answer, 5182078)
+})
+
+test_that("day 2 part 1 returns the correct answer", {
+  answer <- day_02_part1()
+  expect_equal(answer, 3790645)
+})
+
+test_that("day 2 part 2 returns the correct answer", {
+  answer <- day_02_part2()
+  expect_equal(answer, 6577)
 })
